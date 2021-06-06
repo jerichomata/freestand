63b254c9bb5c14fab62559cdea4bf74345ef5128
piercepatrick <piercepatrick2001@gmail.com>
2021-06-06 12:42:30 -0500

    Added grid to nav bar. Spaced components in nav bar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,38 +2,44 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import SearchBar from "material-ui-search-bar";
 import Typography from "@material-ui/core/Typography";
-import { Toolbar } from "@material-ui/core";
+import { FormHelperText, Toolbar } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import {
-  createMuiTheme,
-  withStyles,
-  makeStyles,
-  ThemeProvider,
-} from "@material-ui/core/styles";
-
+import Grid from '@material-ui/core/Grid';
 export default function NavBar() {
   return (
     <AppBar>
       <Toolbar>
-        <Typography style={{ fontWeight: 700, fontSize: 40 }}>
-          Freestand
-        </Typography>
-        <SearchBar
-        //value={this.state.value}
-        //onChange={(newValue) => this.setState({ value: newValue })}
-        //onRequestSearch={() => doSomethingWith(this.state.value)}
-        />
-        <Button
-          style={{
-            fontFamily: [['"Montserrat"', "Open Sans"].join(",")],
-            fontWeight: 400,
-            fontSize: 16,
-            textTransform: "none",
-          }}
-          color="secondary"
+        <Grid
+          justify="space-around"
+          alignItems="center"
+          container
+          
         >
-          Login
-        </Button>
+          <Grid item>
+            <Typography style={{ fontWeight: 700, fontSize: 40 }}>
+              Freestand
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <SearchBar
+            //value={this.state.value}
+            //onChange={(newValue) => this.setState({ value: newValue })}
+            //onRequestSearch={() => doSomethingWith(this.state.value)}
+            />
+          </Grid>
+          <Button
+            style={{
+              fontFamily: [['"Montserrat"', "Open Sans"].join(",")],
+              fontWeight: 400,
+              fontSize: 16,
+              textTransform: "none",
+            }}
+            color="secondary"
+          >
+            Login
+          </Button>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
