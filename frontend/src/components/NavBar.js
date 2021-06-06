84572fe3e350e58ef4b8dc387851f88465659051
piercepatrick <piercepatrick2001@gmail.com>
2021-06-06 12:18:07 -0500

    Style login button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,18 +4,36 @@ import SearchBar from "material-ui-search-bar";
 import Typography from "@material-ui/core/Typography";
 import { Toolbar } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 
 export default function NavBar() {
   return (
     <AppBar>
       <Toolbar>
-        <Typography style={{ fontWeight: 700, fontSize: 40, }}>Freestand</Typography>
+        <Typography style={{ fontWeight: 700, fontSize: 40 }}>
+          Freestand
+        </Typography>
         <SearchBar
         //value={this.state.value}
         //onChange={(newValue) => this.setState({ value: newValue })}
         //onRequestSearch={() => doSomethingWith(this.state.value)}
         />
-        <Button color="secondary">Login</Button>
+        <Button
+          style={{
+            fontFamily: [['"Montserrat"', "Open Sans"].join(",")],
+            fontWeight: 400,
+            fontSize: 16,
+            textTransform: "none",
+          }}
+          color="secondary"
+        >
+          Login
+        </Button>
       </Toolbar>
     </AppBar>
   );
