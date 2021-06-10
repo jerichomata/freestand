f0678a982a3bd1a9d7e76d9b3fd65f01163eb219
piercepatrick <piercepatrick2001@gmail.com>
2021-06-10 09:11:37 -0500

    Removed elevation from navbar. Made it more mobile responsive.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,30 +5,47 @@ import Typography from "@material-ui/core/Typography";
 import { FormHelperText, Toolbar } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  appBar: {
+    position: "relative",
+  },
+  freestandGridItem: {
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center",
+    },
+  },
+  freestandName: {
+    fontWeight: 700,
+    fontSize: 35,
+  },
+  loginButton: {
+    fontFamily: [['"Montserrat"', "Open Sans"].join(",")],
+    fontWeight: 400,
+    fontSize: 16,
+    textTransform: "none",
+  },
+}));
+
 export default function NavBar() {
+  const classes = useStyles();
   return (
     <Fragment>
-      <AppBar style={{ position: "relative" }}>
+      <AppBar className={classes.appBar} elevation={0}>
         <Toolbar>
           <Grid alignItems="center" container>
-            <Grid item md={8}>
-              <Typography style={{ fontWeight: 700, fontSize: 35 }}>
+            <Grid item xs={12} md={8} className={classes.freestandGridItem}>
+              <Typography className={classes.freestandName}>
                 Freestand
               </Typography>
             </Grid>
-            <Grid item md={3}>
+            <Grid item xs={12} md={3}>
               <SearchBar></SearchBar>
             </Grid>
-            <Grid item md={1} align="center">
-              <Button
-                style={{
-                  fontFamily: [['"Montserrat"', "Open Sans"].join(",")],
-                  fontWeight: 400,
-                  fontSize: 16,
-                  textTransform: "none",
-                }}
-                color="secondary"
-              >
+            <Grid item xs={12} md={1} align="center">
+              <Button className={classes.loginButton} color="secondary">
                 Login
               </Button>
             </Grid>
